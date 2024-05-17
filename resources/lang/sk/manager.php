d52a13cd411ec39d1d6fe67c09840915ec805960
Ján Hamrák <snickom@gmail.com>
2024-05-17 12:47:42 +0200

    Slovak translation

---
+++ resources/lang/sk/manager.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'active' => 'Aktívne',
+    'expired' => 'Vypršalo',
+    'unlock' => 'Odomknúť',
+    'unlocked' => 'Odomknutý zdroj',
+    'unlocked_selected' => 'Odomknuté vybrané zdroje',
+    'unlock_all' => 'Odomknúť všetky zdroje',
+];
