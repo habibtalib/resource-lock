05c8e5be67607a628e52291bbc9b637262218794
Muna El-Boashi <45367528+MunaALB@users.noreply.github.com>
2024-07-01 11:34:43 +0200

    Update ManageResourceLocks.php (#54)
    * Update ManageResourceLocks.php
    
    the make function doesnot respond well to arabic words spacing so we need to use the label in here to make it work
    
    * Update ManageResourceLocks.php
    
    edit for the previous change

--- src/Resources/LockResource/ManageResourceLocks.php
+++ src/Resources/LockResource/ManageResourceLocks.php
@@ -15,6 +15,7 @@ class ManageResourceLocks extends ManageRecords
     {
         return [
             Action::make(__('resource-lock::manager.unlock_all'))
+            ->label(__('resource-lock::manager.unlock_all'))
                 ->icon('heroicon-o-lock-open')
                 ->action(fn () => config('resource-lock.models.ResourceLock', ResourceLock::class)::truncate())
                 ->requiresConfirmation(),
